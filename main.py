7c52d04b510abdfb3bdce1a324a5204d9c612807
gr1511 <grosenberg09@gmail.com>
2026-06-05 09:23:37 -0400

    Second Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+from constants import *
+
+def main():
+    print("Starting Asteroids with pygame version:" + pygame.version.ver)
+    print("Screen width: " + str(SCREEN_WIDTH))
+    print("Screen height: " + str(SCREEN_HEIGHT))
+
+
+if __name__ == "__main__":
+    main()
